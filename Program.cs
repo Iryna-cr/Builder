0ccbba66f3be2ff08f12f3447861fdc9b5ac3ae0
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 21:21:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Bouquet
+{
+    public string Flowers;
+    public string Wrapper;
+    public string Ribbon;
+
+    public void Show() => Console.WriteLine($"{Flowers}, {Wrapper}, {Ribbon}");
+}
+
+class BouquetBuilder
+{
+    private Bouquet b = new Bouquet();
+
+    public void AddFlowers() => b.Flowers = "Троянди ";
+    public void AddWrapper() => b.Wrapper = "Паперове пакування ";
+    public void AddRibbon() => b.Ribbon = "Червона стрічка ";
+    public Bouquet GetBouquet() => b;
+}
+
+class Program
+{
+    static void Main()
+    {
+        var builder = new BouquetBuilder();
+        builder.AddFlowers();
+        builder.AddWrapper();
+        builder.AddRibbon();
+
+        var bouquet = builder.GetBouquet();
+        bouquet.Show();
+    }
+}
+
+
+
+
+
